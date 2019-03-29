39c93de5a5863f0a3b4b4421779f8d497f3961c0
Carlos Henrique <carloshpds@gmail.com>
2019-03-29 05:52:25 -0300

    New pt messages

--- src/shared/i18n/messages.js
+++ src/shared/i18n/messages.js
@@ -3,22 +3,26 @@ export const ptBr = {
   seeAnyway: 'Ver notícia mesmo assim',
   negativeReasons: {
     severity_1: {
-      1: 'Ocultei por não agregar ao teu dia',
-      2: 'Chega de negatividade'
+      1: 'Ocultei por não agregar ao teu dia.',
+      2: 'Chega de negatividade.',
+      3: 'Se eu fosse você, olharia outra notícia.',
+      4: 'Tem melhores, mas se quiser tentar...'
     },
     severity_2: {
-      1: 'Lembre-se: Existem coisas boas, não é preciso ver desgraças a todo momento',
-      2: 'Desnecessário'
+      1: 'Lembre-se: Existem coisas boas, não é preciso ver desgraças a todo momento.',
+      2: 'Desnecessário.',
+      3: 'Não perde seu tempo bb.',
+      4: 'Era melhor ter ido assistir um filme.'
     },
 
     severity_3: {
-      1: 'Frase 3',
-      2: 'Frase 3.1'
+      1: 'Não vai acrescentar nada.',
+      2: 'Se eu fosse você não leria.'
     },
 
     severity_4: {
-      1: 'Frase 4',
-      2: 'Frase 4.1'
+      1: 'Deuzolivre.',
+      2: 'Mi-se-ri-cor-dia.'
     },
 
     severity_5: {
