e41c426d654fad2060f07a9ee76b5dd5d1167fa4
Carlos Henrique <carloshpds@gmail.com>
2019-03-29 06:16:12 -0300

    Isolating jquery plugins

--- webpackEntries/content.js
+++ webpackEntries/content.js
@@ -1,21 +1,11 @@
+import '../src/shared/plugins/jqueryChange'
 import MainContentAnalyzer from '../src/contentScripts/main.content'
 import BadBlockSentiment from '../src/shared/AFINN/sentiment'
-import jquery from 'jquery'
 import I18n from '../src/shared/i18n/i18n'
 // import fontawesome from '../src/shared/fontawesome/fontawesome.scss'
 
-(function($) {
-  $.fn.change = function(cb, e) {
-    e = e || { subtree:true, childList:true, characterData:true };
-    $(this).each(function() {
-      function callback(changes) { cb.call(node, changes, this); }
-      var node = this;
-      (new MutationObserver(callback)).observe(node, e);
-    });
-  };
-})(jquery)
-
-window.jQuery = jquery
-
+// ===
+// Run
+// ===
 I18n.discoverMessagesByLocale()
 new MainContentAnalyzer()
