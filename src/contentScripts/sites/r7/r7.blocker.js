836f6d233089798912fe6dd7002ffda586e2482b
Carlos Silva <carloshpds@gmail.com>
2019-03-29 19:41:50 -0300

    Adding basic support for r7 portal

--- src/contentScripts/sites/r7/r7.blocker.js
+++ src/contentScripts/sites/r7/r7.blocker.js
@@ -1,8 +1,10 @@
 import DOMBadBlocker from "../../../shared/DOMBadBlocker/DOMBadBlocker";
 import g1Style from './r7.style.scss'
+import $ from 'jquery'
 
 export default class R7SiteBlocker {
   constructor(sentiment) {
+    $('body').addClass('r7-bad-blocker-helper')
 
     const diverseTrendsSettings = {
       name: 'r7-diverse-trends' ,
@@ -60,11 +62,42 @@ export default class R7SiteBlocker {
       }
     }
 
+    const topNewsSettings = {
+      name: 'r7-top-news' ,
+
+      selectors: {
+        rootPostsWatcher: '.mais-lidas-trends',
+        post: {
+          wrapper : 'itself',
+          itself  :'.mais-lidas-trends-title',
+          title   : 'itself',
+          subtitle: undefined
+        }
+      }
+    }
+
+    const sidebarTopNewsSettings = {
+      name: 'r7-sidebar-top-news' ,
+
+      selectors: {
+        rootPostsWatcher: '.box.mais_lidas_sidebar_trends .mais-lidas-trends',
+        post: {
+          wrapper : 'itself',
+          itself  :'.mais-lidas-trends-title',
+          title   : 'itself',
+          subtitle: undefined
+        }
+      }
+    }
+
+
 
     this.diverseTrends        = new DOMBadBlocker(diverseTrendsSettings)
     this.carouselNews         = new DOMBadBlocker(carouselNewsSettings)
     this.twoHighlightedTrends = new DOMBadBlocker(twoHighlightedTrendsSettings)
     this.diverseCallTrends    = new DOMBadBlocker(diverseCallTrendsSettings)
+    this.topNews              = new DOMBadBlocker(topNewsSettings)
+    this.sidebarTopNews       = new DOMBadBlocker(sidebarTopNewsSettings)
 
   }
 
