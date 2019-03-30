1bde7be9f1736f2355b04532cb7bfdf950b23961
Carlos Henrique <carloshpds@gmail.com>
2019-03-30 13:43:12 -0300

    Adding ChromeExtensionReloader to fix hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
-var webpack = require("webpack"),
+const webpack = require("webpack"),
     path = require("path"),
     fileSystem = require("fs"),
     env = require("./utils/env"),
     CleanWebpackPlugin = require("clean-webpack-plugin"),
     CopyWebpackPlugin = require("copy-webpack-plugin"),
     HtmlWebpackPlugin = require("html-webpack-plugin"),
-    WriteFilePlugin = require("write-file-webpack-plugin");
+    WriteFilePlugin = require("write-file-webpack-plugin"),
+    ChromeExtensionReloader  = require('webpack-chrome-extension-reloader');
 
 // load the secrets
-var alias = {};
+const alias = {};
 
-var secretsPath = path.join(__dirname, ("secrets." + env.NODE_ENV + ".js"));
+const secretsPath = path.join(__dirname, ("secrets." + env.NODE_ENV + ".js"));
 
-var fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"];
+const fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"];
 
 if (fileSystem.existsSync(secretsPath)) {
   alias["secrets"] = secretsPath;
@@ -94,7 +95,9 @@ var options = {
       chunks: ["popupApp"]
     }),
 
-    new WriteFilePlugin()
+    new WriteFilePlugin(),
+
+    new ChromeExtensionReloader()
   ]
 };
 
