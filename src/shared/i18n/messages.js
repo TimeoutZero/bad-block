e67cb4d68bb4462ade270a10490d2a5aad3cdf4e
Carlos Henrique <carloshpds@gmail.com>
2019-03-31 02:45:09 -0300

    New messages for pt language

--- src/shared/i18n/messages.js
+++ src/shared/i18n/messages.js
@@ -6,36 +6,46 @@ export const ptBr = {
   seeAnyway: 'Ver notícia mesmo assim',
   negativeReasons: {
     severity_1: {
-      description: 'Nível de negatividade 1: Notícias com informações gerais após acidentes',
+      description: 'Nível de negatividade 1: Possíveis notícias com informações gerais após acidentes',
+
       1: 'Ocultei por não agregar ao teu dia.',
       2: 'Chega de negatividade.',
       3: 'Se eu fosse você, olharia outra notícia.',
       4: 'Tem melhores, mas se quiser tentar...'
     },
     severity_2: {
-      description: 'Nível de negatividade 2: Notícias com possíveis mortes',
-      1: 'Lembre-se: Existem coisas boas, não é preciso ver desgraças a todo momento.',
-      2: 'Desnecessário.',
-      3: 'Não perde seu tempo bb.',
-      4: 'Era melhor ter ido assistir um filme.'
+      description: 'Nível de negatividade 2: Possíveis notícias com mortes acidentais',
+
+      1: 'Existem coisas boas, não é preciso ver desgraças a todo momento.',
+      2: 'De tristezas o mundo já tá cheio. Leia outra notícia.',
+      3: 'Não vale a pena ler notícias tristes como esta.',
+      4: 'Era melhor ter ido assistir um filme.',
+      5: 'Atualize-se sobre coisas boas. Esta notícia traz consigo apenas mais drama.'
     },
 
     severity_3: {
-      description: 'Nível de negatividade 3: ',
+      description: 'Nível de negatividade 3: Possíveis notícias de assassinato',
+
       1: 'Não vai acrescentar nada.',
-      2: 'Se eu fosse você não leria.'
+      2: 'Prefiro não te contar sobre o que aconteceu.'
     },
 
     severity_4: {
-      description: 'Nível de negatividade 4: Notícias de protestos.',
+      description: 'Nível de negatividade 4: Possíveis notícias de protestos, crimes generalizados, ',
+
       1: 'Deuzolivre.',
-      2: 'Mi-se-ri-cor-dia.'
+      2: 'Não clique, você pode se arrepender.',
+      3: 'Desgraça? Nem de graça.',
+      4: 'Notícia sobre atrocidade absurda não devia nem ser veiculada.'
     },
 
     severity_5: {
-      description: 'Nível de negatividade 5: Notícias de tragédias com fatalidades, desastres naturais ou erros humanos.',
-      1: 'Frase 5',
-      2: 'Frase 5.1'
+      description: 'Nível de negatividade 5: Possíveis notícias de tragédias com fatalidades, desastres naturais ou erros humanos.',
+
+      1: 'Desgraça? Nem de graça.',
+      2: 'Notícia com alto grau de atrocidade.',
+      3: 'Cuidado para não se sentir pior com notícias como esta.',
+      4: 'Conteúdo de estímulo negativo ao leitor.'
     }
   }
 };
